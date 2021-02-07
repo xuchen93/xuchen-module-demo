a77020b976318035e43ed8e53496b016a39abb94
edwin.wang <edwin.wang@movit-tech.com>
2021-02-07 17:57:35 +0800

    调试中

--- server-demo/src/main/java/com/github/xuchen93/ThreadLocalScope.java
+++ server-demo/src/main/java/com/github/xuchen93/ThreadLocalScope.java
@@ -1,39 +1,36 @@
 package com.github.xuchen93;
 
-import cn.hutool.core.thread.ThreadUtil;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author edwin
  */
 public class ThreadLocalScope implements Scope {
-	private static final ThreadLocal<Map<String,Object>> THREAD_LOCAL_SCOPE;
+	private static final ThreadLocal THREAD_LOCAL_SCOPE;
+
 	static {
 		THREAD_LOCAL_SCOPE = new ThreadLocal<>();
-		THREAD_LOCAL_SCOPE.set(new HashMap<>());
+
 	}
 
 	@Override
 	public Object get(String name, ObjectFactory<?> objectFactory) {
 		System.out.println("----get对象----");
-		Object value = THREAD_LOCAL_SCOPE.get().get(name);
+		Object value = THREAD_LOCAL_SCOPE.get();
 		if (value != null) {
 			return value;
 		}
 		Object object = objectFactory.getObject();
-		THREAD_LOCAL_SCOPE.get().put(name,object);
+		THREAD_LOCAL_SCOPE.get();
 		return object;
 	}
 
 	@Override
 	public Object remove(String name) {
 		System.out.println("----销毁对象----");
-		return THREAD_LOCAL_SCOPE.get().remove(name);
+		THREAD_LOCAL_SCOPE.remove();
+		return null;
 	}
 
 	@Override
